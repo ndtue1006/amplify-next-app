800e6b077b978f981ba36ac0f44fc6a9e1b7e322
Tue Nguyen <tue.nd@techchain.vn>
2021-10-26 22:43:55 +0700

    Test update message

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello world</h1>
       <main className={styles.main}>
+        <h1>Hello world</h1>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
